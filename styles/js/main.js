615bc6e985b06ebe11afec5a69a873e4ce4232f2
Christine Liu <Christine@Christines-MacBook-Pro-2.local>
2014-04-06 16:25:28 -0700

    first commit

---
+++ styles/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#nav').onePageNav({
+         currentClass: 'active',
+         changeHash: true,
+         scrollSpeed: 1200
+    });
+
+	// Init Skrollr
+	var s = skrollr.init();
+	 
+	// Refresh Skrollr after resizing our sections
+	s.refresh($('#landing'));
+ });
+
