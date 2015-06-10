a0148f5ba16cfb568c5329c9311264fc79afd969
Christine Liu <winter108@gmail.com>
2015-06-09 21:29:59 -0700

    analytics

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+  var appInsights=window.appInsights||function(config){
+        function s(config){t[config]=function(){var i=arguments;t.queue.push(function(){t[config].apply(t,i)})}}var t={config:config},r=document,f=window,e="script",o=r.createElement(e),i,u;for(o.src=config.url||"//az416426.vo.msecnd.net/scripts/a/ai.0.js",r.getElementsByTagName(e)[0].parentNode.appendChild(o),t.cookie=r.cookie,t.queue=[],i=["Event","Exception","Metric","PageView","Trace"];i.length;)s("track"+i.pop());return config.disableExceptionTracking||(i="onerror",s(""+i),u=f[i],f[i]=function(config,r,f,e,o){var s=u&&u(config,r,f,e,o);return s!==!0&&t[""+i](config,r,f,e,o),s}),t
+    }({
+        instrumentationKey:"12ec713d-fd60-46a0-965f-7ceb021787a7"
+    });
+    
+    window.appInsights=appInsights;
+    appInsights.trackPageView();
+
   window.mobilecheck = function() {
     if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i))
       {
